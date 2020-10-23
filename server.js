7ab723772e77444bb749261c79715900436d9c78
Sai Vinay M <maheshwaram.saivinay@gmail.com>
2020-10-23 17:07:55 +0530

    secound push

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-if(process.env.NODE_ENV !== 'production'){
+/* git commands to push to git and heroku
+    git add .
+    git commit -m "-m means message and name it here"
+    git push
+    git push heroku master
+*/
+
+if(process.env.NODE_ENV != 'production'){
     require('dotenv').config()
 }
 const express = require("express")
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views');
 app.set('layout','layouts/layout')
 app.use(expressLayouts);
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true,useUnifiedTopology: true})
@@ -21,6 +31,7 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('connected'));
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 
 app.listen(process.env.PORT || 3000)
